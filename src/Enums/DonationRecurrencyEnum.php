e96db29624c0fe2bb0e287b9c535fb2a686779ee
d4rk0s <gregory@d4rk0s.net>
2022-05-17 12:16:12 +0200

    bugfix

--- src/Enums/DonationRecurrencyEnum.php
+++ src/Enums/DonationRecurrencyEnum.php
@@ -4,14 +4,14 @@ namespace D4rk0snet\Donation\Enums;
 
 enum DonationRecurrencyEnum : string
 {
-    case MONTHLY = 'monthly';
-    case ONESHOT = 'oneshot';
+case MONTHLY = 'monthly';
+case ONESHOT = 'oneshot';
 
     public function getStripeProductId()
     {
-        return match($this) {
-            DonationRecurrencyEnum::MONTHLY => 'xxx',
-            DonationRecurrencyEnum::ONESHOT => 'xxx'
+        return match ($this) {
+            DonationRecurrencyEnum::MONTHLY => getenv('STRIPE_MODE') === 'test' ? 'prod_LhmfY9jyxdr3wK' : 'prod_LS3idKzaxwfh2n',
+            DonationRecurrencyEnum::ONESHOT => getenv('STRIPE_MODE') === 'test' ? 'prod_LVr0bZLDLS6lPP' : 'prod_LS3EGi1t7qWSGP'
         };
     }
 }
