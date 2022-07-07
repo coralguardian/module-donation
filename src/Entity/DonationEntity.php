205073f4da95e6fd25e05572fe2521aeef08efb3
d4rk0s <gregory@d4rk0s.net>
2022-07-07 17:13:54 +0200

    Passage en uuid standard

--- src/Entity/DonationEntity.php
+++ src/Entity/DonationEntity.php
@@ -26,7 +26,7 @@ class DonationEntity
      * @ORM\Id
      * @ORM\Column(type="uuid_binary_ordered_time", unique=true)
      * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidOrderedTimeGenerator")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
      */
     private $uuid;
 
