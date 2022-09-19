2afb395afa73d4d2f6d08ee3c966394c0b705db7
d4rk0s <gregory@d4rk0s.net>
2022-09-19 11:53:14 +0200

    bugfix

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,12 +2,9 @@
 
 namespace D4rk0snet\Donation;
 
-use D4rk0snet\Donation\API\DonateEndpoint;
-
 class Plugin
 {
     public static function init()
     {
-        do_action(\Hyperion\RestAPI\Plugin::ADD_API_ENDPOINT_ACTION, new DonateEndpoint());
     }
 }
