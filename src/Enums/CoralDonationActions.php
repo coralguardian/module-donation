550e1c893002332ffcc93a916ec945c47f0da4af
Gregory Collin <gregory@d4rk0s.net>
2022-11-08 17:29:30 +0100

    Fix donation event sending

--- src/Enums/CoralDonationActions.php
+++ src/Enums/CoralDonationActions.php
@@ -5,5 +5,5 @@ namespace D4rk0snet\Donation\Enums;
 enum CoralDonationActions : string
 {
     case PENDING_DONATION = 'coraldonation_pending_donation';
-    case NEW_DONATION = 'coraldonation_new_donation';
+    case DONATION_CREATED = 'coraldonation_new_donation';
 }
--- src/Listener/NewDonation.php
+++ src/Listener/NewDonation.php
@@ -8,17 +8,16 @@ use D4rk0snet\CoralOrder\Enums\PaymentMethod;
 use D4rk0snet\CoralOrder\Enums\Project;
 use D4rk0snet\CoralOrder\Model\DonationOrderModel;
 use D4rk0snet\Donation\Enums\CoralDonationActions;
-use D4rk0snet\Donation\Enums\DonationRecurrencyEnum;
 use D4rk0snet\Donation\Models\DonationModel;
 use JsonMapper;
 
 /**
  * Cette classe écoute l'action NEW_MONTHLY_SUBSCRIPTION du module order
  */
-class NewSubscription
+class NewDonation
 {
     public static function doAction(
-        DonationOrderModel $monthlyDonation,
+        DonationOrderModel $donationOrderModel,
         CustomerModel $customerModel,
         string $setupIntentId
     ){
@@ -28,13 +27,13 @@ class NewSubscription
 
             $donationModel = new DonationModel();
             $donationModel
-                ->setDonationRecurrency(DonationRecurrencyEnum::MONTHLY)
-                ->setAmount($monthlyDonation->getAmount())
+                ->setDonationRecurrency($donationOrderModel->getDonationRecurrency())
+                ->setAmount($donationOrderModel->getAmount())
                 ->setStripePaymentIntentId($setupIntentId) // @todo: Changer en base ce n'est pas un paymentIntentId
                 ->setIsPaid(true)
                 ->setDate(new \DateTime())
                 ->setPaymentMethod(PaymentMethod::CREDIT_CARD)
-                ->setProject(Project::from($monthlyDonation->getProject()))
+                ->setProject(Project::from($donationOrderModel->getProject()))
                 ->setLang(Language::FR) // @todo: Est ce que la langue a réellement du sens dans un don mensuel?
                 ->setCustomerModel($customerModel);
 
